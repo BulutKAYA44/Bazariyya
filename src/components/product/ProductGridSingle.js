8e147983c1a1613ca892bf94eaa5de4021b22a77
BulutKAYA <bulutkaya67@gmail.com>
2024-05-25 23:28:52 +0300

    product chart edit

--- src/components/product/ProductGridSingle.js
+++ src/components/product/ProductGridSingle.js
@@ -3,7 +3,6 @@ import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import priceFormat from "../../helpers/priceFormatter";
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 
@@ -13,8 +12,9 @@ const ProductGridSingle = ({
   spaceBottomClass,
   newProduct,
 }) => {
-
   const [wishlistCount, setWishlistCount] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
 
   const handleWishlistClick = (action) => {
     if (action === "increase") {
@@ -24,46 +24,94 @@ const ProductGridSingle = ({
     }
   };
 
+  const handleQuantityChange = (action) => {
+    if (action === "increase") {
+      setQuantity((prevQuantity) => prevQuantity + 1);
+    } else if (action === "decrease" && quantity > 0) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (/^\d*$/.test(value)) {
+      setQuantity(value === "" ? 0 : parseInt(value, 10));
+    }
+  };
+
+  const handleFocus = () => {
+    setShowPlaceholder(false);
+  };
+
+  const handleBlur = () => {
+    if (quantity === 0) {
+      setShowPlaceholder(true);
+    }
+  };
+
   return (
     <Fragment>
-    <div className={`col-xl-4 col-md-6 col-lg-4 col-sm-6 mb-4 ${sliderClassName ? sliderClassName : ""}`}>
-      <div className={`product-wrap ${spaceBottomClass ? spaceBottomClass : ""}`}>
-        <div className="product-img">
-          <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
-            <img
-              className="default-img"
-              src={product.productImages[0]?.url}
-              alt={product.name}
-            />
-            {newProduct && (
-              <div className="product-img-badges">
-                <span className="badge badge-new">Yeni</span>
-              </div>
-            )}
-          </Link>
-        </div>
-        <div className="product-content">
-          <h3 className="product-title">
+      <div className={`col-xl-4 col-md-6 col-lg-4 col-sm-6 mb-4 ${sliderClassName ? sliderClassName : ""}`}>
+        <div className={`product-wrap ${spaceBottomClass ? spaceBottomClass : ""}`}>
+          <div className="product-img">
             <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
-              {product.name}
-            </Link>
-          </h3>
-          <div className="product-price">
-            <span>{priceFormat(product.price)}</span>
-          </div>
-          <div className="product-actions">
-            <button className="btn btn-primary add-to-cart-btn">Sepete Ekle</button>
-            <Button>+</Button>
-              <TextField
-                id="filled-start-adornment"
-                defaultValue="kg"
-                size="small"
+              <img
+                className="default-img"
+                src={product.productImages[0]?.url}
+                alt={product.name}
               />
-              <Button>-</Button>
+              {newProduct && (
+                <div className="product-img-badges">
+                  <span className="badge badge-new">Yeni</span>
+                </div>
+              )}
+            </Link>
+          </div>
+          <div className="product-content">
+            <h3 className="product-title">
+              <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
+                {product.name}
+              </Link>
+            </h3>
+            <div className="product-price">
+              <span>{priceFormat(product.price)}</span>
+            </div>
+            <div className="product-actions">
+              <button className="btn btn-primary add-to-cart-btn">Sepete Ekle</button>
+              <div className="wishlist-actions">
+                <Button
+                  variant="contained"
+                  onClick={() => handleQuantityChange("increase")}
+                >
+                  +
+                </Button>
+                <TextField
+                  value={quantity === 0 ? "" : quantity}
+                  onChange={handleInputChange}
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}
+                  placeholder={showPlaceholder ? "kg" : ""}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {showPlaceholder ? "" : ""}
+                      </InputAdornment>
+                    ),
+                    style: showPlaceholder ? { color: "lightgrey" } : {}
+                  }}
+                  size="small"
+                />
+                <Button
+                  variant="contained"
+                  onClick={() => handleQuantityChange("decrease")}
+                >
+                  -
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Fragment>
   );
 };
